7872d70b8ad8cb3e86693e8e34e7da7e54915d7e
moosa <moosashaik683@gmail.com>
2024-07-09 22:18:52 +0530

    first commit

--- cypress/e2e/form_submit.cy.js
+++ cypress/e2e/form_submit.cy.js
@@ -1,35 +1,36 @@
+const jsondata = require('../fixtures/data.json')
+
 describe('form submit test suite', () => {
-  before(function() {
-    cy.fixture('data').then(function(data) {
 
-      globalThis.data = data;
+  jsondata.forEach((data)  => {
 
+    it('Should submit successfully', () => {
+
+      cy.log("Test Starting...");
+      cy.visit('https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform');
+      cy.log("Form opened");
+  
+      cy.xpath('//input[@type="text"]').type(data.name);
+      cy.log("Name entered");
+  
+      cy.get(data.age).click();
+      cy.log(data.comment1);
+  
+      cy.get('div[role="listbox"]').click();
+      cy.get(data.chooseAoption).click();
+      cy.log(data.comment2);
+  
+      cy.get('div[role="button"]').contains('Submit').click();
+      cy.log("Clicked on Submit");
+  
+      cy.contains('Your response has been recorded.');
+      cy.log("Confirmed Response");
+      
+      //cy.xpath('//a[text()="Submit another response"]').click();
+      
     })
-  })
-
-  it('Should submit successfully', () => {
-
-    cy.log("Test Starting...");
-    cy.visit('https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform');
-    cy.log("Form opened");
-
-    cy.xpath('//input[@type="text"]').type(data.name);
-    cy.log("Name entered");
-
-    cy.get(data.age).click();
-    cy.log(data.comment1);
-
-    cy.get('div[role="listbox"]').click();
-    cy.get(data.chooseAoption).click();
-    cy.log(data.comment2);
-
-    cy.get('div[role="button"]').contains('Submit').click();
-    cy.log("Clicked on Submit");
-
-    cy.contains('Your response has been recorded.');
-    cy.log("Confirmed Response");
     
-    //cy.xpath('//a[text()="Submit another response"]').click();
-    
-  })
+  });
+
+  
 })
